fe37ead5f204cf09f9ac1d6c647a2befc01426e3
AndKrogh <anders.krogh99@gmail.com>
2022-03-13 19:46:01 +0100

    bla bla

--- Script.js
+++ Script.js
@@ -1,9 +1,21 @@
-var x = 2
 
 
 // Anders javascript
 
 
+let hamburger = document.querySelector(".hamburger");
+let navMenu = document.querySelector(".nav-menu");
+
+const hamburgerMenu = [hamburger, navMenu];
+
+hamburger.addEventListener('click', function(){
+    hamburgerMenu[0,1].classList.toggle("active");
+});
+
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener('click', function() {
+    hamburgerMenu[0, 1].classList.remove("active");
+}));
 
 
 
