9000878df158b7f8a51fc2175b4a00dba734a86f
AndKrogh <anders.krogh99@gmail.com>
2022-03-14 11:32:19 +0100

    Update Script.js

--- Script.js
+++ Script.js
@@ -16,6 +16,18 @@ document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", 
     navMenu.classList.remove("active");
 }));
 
+function forsideKnapLinks (){
+    document.location.href="feed.html";
+}
+
+function forsideKnapLinks1 (){
+    document.location.href="fundetnoget.html";
+}
+
+function forsideKnapLinks2 (){
+    document.location.href="reservation.html";
+}
+
 /* let hamburger = document.querySelector(".hamburger");
 let navMenu = document.querySelector(".nav-menu");
 
