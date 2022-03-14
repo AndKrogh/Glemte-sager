8e86e547147950b6a764f76b8cb7781902a5fc85
emmasalling <emmasalling4@gmail.com>
2022-03-14 13:03:10 +0100

    Update Script.js

--- Script.js
+++ Script.js
@@ -16,6 +16,21 @@ document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", 
     navMenu.classList.remove("active");
 }));
 
+function forsideKnapLinks (){
+    document.location.href="feed.html";
+}
+
+function forsideKnapLinks1 (){
+    document.location.href="fundetnoget.html";
+}
+
+function forsideKnapLinks2 (){
+    document.location.href="registrergenstand.html";
+}
+
+function feedKnap (){
+  document.location.href="reservation.html";
+}
 /* let hamburger = document.querySelector(".hamburger");
 let navMenu = document.querySelector(".nav-menu");
 
@@ -36,11 +51,70 @@ document.querySelectorAll(".nav-link").forEach(n => n.addEventListener('click', 
 // Peter javascript
 
 
+let faq = document.getElementsByClassName("faq-page");
+let i;
+for (i = 0; i < faq.length; i++) {
+    faq[i].addEventListener("click", function () {
+        /* Toggle between adding and removing the "active" class,
+        to highlight the button that controls the panel */
+        this.classList.toggle("active");
+        /* Toggle between hiding and showing the active panel */
+        let body = this.nextElementSibling;
+        if (body.style.display === "block") {
+            body.style.display = "none";
+        } else {
+            body.style.display = "block";
+        }
+    });
+}
+//Get the button:
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
 
 
 //Jack javascript
 
-
+function validate(){
+    let name = document.getElementById("name").value;
+    let subject = document.getElementById("subject").value;
+    let message = document.getElementById("message").value;
+    let error_message = document.getElementById("error_message");
+  
+    error_message.style.padding = "10px";
+  
+    let text;
+    if(name.length < 5){
+      text = "giv genstanden et navn jooo";
+      error_message.innerHTML = text;
+      return false;
+    }
+  
+  
+    if(message.length <= 5){
+      text = "tilføj lidt beskrivelse fætter guf";
+      error_message.innerHTML = text;
+      return false;
+    }
+    alert("det gik krattedeme godt!! ");
+    return true;
+  }
 
 
 
