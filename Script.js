e85b2dd2437ff698b3141d0792fdac4fa615e804
HeiseP <95848120+HeiseP@users.noreply.github.com>
2022-03-14 11:40:56 +0100

    forsideFaqs

--- Script.js
+++ Script.js
@@ -48,6 +48,42 @@ document.querySelectorAll(".nav-link").forEach(n => n.addEventListener('click', 
 // Peter javascript
 
 
+let faq = document.getElementsByClassName("faq-page");
+let i;
+for (i = 0; i < faq.length; i++) {
+    faq[i].addEventListener("click", function () {
+        /* Toggle between adding and removing the "active" class,
+        to highlight the button that controls the panel */
+        this.classList.toggle("active");
+        /* Toggle between hiding and showing the active panel */
+        let body = this.nextElementSibling;
+        if (body.style.display === "block") {
+            body.style.display = "none";
+        } else {
+            body.style.display = "block";
+        }
+    });
+}
+//Get the button:
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
 
 
 //Jack javascript
