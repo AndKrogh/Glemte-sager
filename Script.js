da2d5c245e5607bdbf8c242abbb201ec768d8cbc
emmasalling <emmasalling4@gmail.com>
2022-03-14 10:38:50 +0100

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,9 +1,34 @@
-var x = 2
 
 
 // Anders javascript
 
+let hamburger = document.querySelector(".hamburger");
+let navMenu = document.querySelector(".nav-menu");
 
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+});
+
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}));
+
+/* let hamburger = document.querySelector(".hamburger");
+let navMenu = document.querySelector(".nav-menu");
+
+const hamburgerMenu = [hamburger, navMenu];
+
+hamburger.addEventListener('click', function(){
+    hamburgerMenu[0,1].classList.toggle("active");
+});
+
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener('click', function() {
+    hamburgerMenu[0, 1].classList.remove("active");
+})); */
 
 
 
@@ -29,3 +54,12 @@ var x = 2
 
 
 //Steph javascript
+
+// const beskrivelse1 = document.getElementById('beskrivelse');
+// const reserver1 = document.getElementsByClassName('reserverKnap');
+// const out1 = document.getElementById('output1');
+//
+// function fun1() {
+//   out1.innerHTML = beskrivelse1.value;
+// }
+// reserver1.addEventListener('click', fun1);
